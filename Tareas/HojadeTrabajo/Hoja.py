07d5ff25a6aedd42365b0a11520b8913ef10f4f2
RMACR7LP <cristianjalvarez123@gmail.com>
2017-10-30 13:11:04 -0600

    Agregando la hoja de trabajo

---
+++ Tareas/HojadeTrabajo/Hoja.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    f=1
+    for i in range(1,n+1):
+        f=f*(i)
+    
+
+for i in range (0,)
+
+#el número no puede tener 8 o más dígitos
+#-------------------------------- hasta aquí hice en clase----------------------------------------------------
